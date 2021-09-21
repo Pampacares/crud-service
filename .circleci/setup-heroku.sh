94b5c23bc8e22d9e1dd13d35bde28f90c4f315fb
Igor Dalepiane da Costa <igordalepiane@gmail.com>
2021-09-21 01:13:31 -0300

    Create setup-heroku.sh

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login igordalepiane@gmail.com
+  password 4fa609ea-7634-4aab-acfc-d4ee72612b2b
+machine git.heroku.com
+  login igordalepiane@gmail.com
+  password 4fa609ea-7634-4aab-acfc-d4ee72612b2b
+EOF
